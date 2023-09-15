7dd964872b1614fdb1cc9268bef89961bded911f
Kari Vigna <karivigna24@gmail.com>
2023-09-15 16:47:20 -0700

    program.cs functions

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -1,7 +1,6 @@
 using System.Collections.Generic;
 using System;
-// using Bread.Models;
-// using Pastry.Models;
+using Bakery.Models;
 
 namespace Bakery
 {
@@ -9,13 +8,15 @@ namespace Bakery
   {
     static void Main()
     {
+      Console.BackgroundColor = ConsoleColor.Blue;
       Console.WriteLine("Welcome to Pierre's Bakery! Our fresh made bread is $5 per loaf and if you buy 2, the 3rd will be free! How many loaves of bread would you like?");
-      int breadAmt = Console.ReadLine();
+      int breadAmt = int.Parse(Console.ReadLine());
       Bread newBread = new Bread(breadAmt);
-      Console.WriteLine("Lovely! Our pastries are $2 each and when you buy 3, the 4th will be free! How many pastries would you like?")
-      int pastryAmt = Console.ReadLine();
+      Console.WriteLine("Lovely! Our pastries are $2 each and when you buy 3, the 4th will be free! How many pastries would you like?");
+      int pastryAmt = int.Parse(Console.ReadLine());
       Pastry newPastry = new Pastry(pastryAmt);
-      Console.WriteLine("Yummy! Your total will be ${Pastry.CalculatePastryCost()} ")
+      Console.WriteLine("Yummy! You ordered " + breadAmt.ToString() + " loafs of bread and " + pastryAmt.ToString() + " pastries!");
+      Console.WriteLine("Your total will be $" + newBread.CalculateBreadCost(breadAmt) + newPastry.CalculatePastryCost(pastryAmt));
     }
   }
 }
